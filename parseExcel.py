a65d2e231f842ccd520482534a0fdef38ab1de14
yaja <yaja@protonmail.com>
2019-09-06 13:56:17 -0400

    refactor

--- parseExcel.py
+++ parseExcel.py
@@ -1,39 +1,61 @@
 import codecs
 import csv
 import json
+import math
 import requests
+import sys
 import xlrd
 
-book = xlrd.open_workbook('se_assignment_users.csv.xlsx')
-sheet = book.sheets()[0]
-header = []
-nrows = sheet.nrows
-ncols = sheet.ncols
-result = []
-for i in range(0, ncols):
-    header.append(sheet.cell(0,i).value)
-for row in range(1,5):
-    dict = {}
-    for col in range(1, ncols):
-        val = sheet.cell(row, col).value
-        dict[header[col]] = val
-    #print json.dumps(dict)
-    result.append(json.dumps(dict))
-print result
-file = codecs.open("customerjsraw", "w", 'utf-8')
-file.write(result)
-file.close()
-jsons = json.dumps(result, ensure_ascii=False)
-file = codecs.open("customerjs", "w", 'utf-8')
-file.write(jsons)
-file.close()
+def parse(file_path):
+    book = xlrd.open_workbook(file_path)
+    sheet = book.sheets()[0]
+    csv_header = []
+    int_header = ['totalTomatoOrders', 'daysSinceLastOrder', 'zip', 'age']
+    nrows = sheet.nrows
+    ncols = sheet.ncols
+    result = []
+    for i in range(0, ncols):
+        csv_header.append(sheet.cell(0,i).value)
+    for row in range(1,nrows):
+        dict = {}
+        for col in range(1, ncols):
+            if csv_header[col] in int_header:
+                val = int(sheet.cell(row, col).value)
+            else:
+                val = sheet.cell(row, col).value
+            dict[csv_header[col]] = val
+        result.append(json.dumps(dict))
 
-headers = {'content-type': 'application-json', 'X-API-KEY': ''}
-for i in range(0, len(jsons)):
-    #print jsons[i]
-    r = requests.post('http://localhost:9000', data=jsons[i], headers=headers)
-for i in range(0, ceil(len(jsons)/50)):
-    left = i*50
-    right = min(i*50+49,len(jsons))
-    print str(left) + (':') + str(right)
-    r = request.post("http://localhost:3999", data=jsons[left:right], headers=headers)
+    # extra - just to save to a file
+    jsons = json.dumps(result, ensure_ascii=False)
+    file = codecs.open("customer_json", "w", 'utf-8')
+    file.write(jsons)
+    file.close()
+
+    return result
+
+def userUpdate(baseUrl, user_json):
+    headers = {'content-type': 'application-json', 'X-API-KEY': ''}
+    print json.dumps(user_json)
+    r = requests.post(baseUrl + '/api/users/update', data=json.dumps(user_json), headers=headers)
+
+def bulkUserUpdate(baseUrl, users_json):
+    headers = {'content-type': 'application-json', 'X-API-KEY': ''}
+    print json.dumps(users_json)
+    r = requests.post(baseUrl + '/api/users/bulkUpdate', data=json.dumps(users_json), headers=headers)
+
+def main():
+    file_path = sys.argv[1]
+    baseUrl = sys.argv[2]
+    parsed_data = parse(file_path)
+
+    #test single Update
+    #for i in range(0,len(parsed_data)):
+        #userUpdate(baseUrl, parsed_data[i])
+    for i in range(0, -(-len(parsed_data)//50)):
+        left = i*50
+        right = min(i*50+49,len(parsed_data))
+        bulkUserUpdate(baseUrl, parsed_data[left:right])
+
+if __name__ == '__main__':
+    main()
