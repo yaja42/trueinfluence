354d6a90601d9d128df373cd3269722c7f266a23
xiaoya88 <yaja@protonmail.com>
2019-09-06 09:30:41 -0400

    refactor

--- parseExcel.py
+++ parseExcel.py
@@ -12,22 +12,28 @@ ncols = sheet.ncols
 result = []
 for i in range(0, ncols):
     header.append(sheet.cell(0,i).value)
-    print header
-for row in range(1,nrows):
+for row in range(1,5):
     dict = {}
     for col in range(1, ncols):
         val = sheet.cell(row, col).value
         dict[header[col]] = val
-    result.append(dict)
+    #print json.dumps(dict)
+    result.append(json.dumps(dict))
+print result
+file = codecs.open("customerjsraw", "w", 'utf-8')
+file.write(result)
+file.close()
 jsons = json.dumps(result, ensure_ascii=False)
 file = codecs.open("customerjs", "w", 'utf-8')
 file.write(jsons)
 file.close()
 
 headers = {'content-type': 'application-json', 'X-API-KEY': ''}
-for i = range(0, len(jsons)):
-    r = requests.post(singleUrl, data=result[i], headers=headers)
-for i = range(0: ceil(len(jsons)/50)):
+for i in range(0, len(jsons)):
+    #print jsons[i]
+    r = requests.post('http://localhost:9000', data=jsons[i], headers=headers)
+for i in range(0, ceil(len(jsons)/50)):
     left = i*50
     right = min(i*50+49,len(jsons))
-    r = request.post(batchUrl, data=[left,right], headers=headers)
+    print str(left) + (':') + str(right)
+    r = request.post("http://localhost:3999", data=jsons[left:right], headers=headers)
